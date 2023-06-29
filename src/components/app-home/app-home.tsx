2319bcf64c8f9874eb5b96ec67228194bd4e06d8
Henning Klages <henning.klages@posteo.de>
2023-06-29 16:37:00 +0200

    first commit

---
+++ src/components/app-home/app-home.tsx
@@ -0,0 +1,29 @@
+import { Component, h, Element } from '@stencil/core';
+
+@Component({
+  tag: 'app-home',
+  styleUrl: 'app-home.css',
+  shadow: true,
+})
+export class AppHome {
+  @Element() el!: HTMLElement;
+
+  connectedCallback() {
+    console.log(this.el);
+  }
+
+  render() {
+    return (
+      <div class="app-home">
+        <p>
+          Welcome to the Stencil App Starter. You can use this starter to build entire apps all with web components using Stencil! Check out our docs on{' '}
+          <a href="https://stenciljs.com">stenciljs.com</a> to get started.
+        </p>
+
+        <stencil-route-link url="/profile/stencil">
+          <button>Profile page</button>
+        </stencil-route-link>
+      </div>
+    );
+  }
+}
